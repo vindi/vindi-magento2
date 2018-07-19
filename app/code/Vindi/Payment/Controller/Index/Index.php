b19af913b2be91746b6d2995ca1ef79a730daddb
leonardo albuquerque <albuquerque.leonardo@trezo.com.br>
2018-07-19 10:23:18 -0300

    #11177 Iniciado criação do meio de pagamento por cartão de crédito

---
+++ app/code/Vindi/Payment/Controller/Index/Index.php
@@ -0,0 +1,25 @@
+<?php
+namespace Vindi\Payment\Controller\Index;
+
+use Vindi\Payment\Model\Api;
+
+class Index extends \Magento\Framework\App\Action\Action
+{
+    protected $_pageFactory;
+
+    public function __construct(
+        Api $api,
+        \Magento\Framework\App\Action\Context $context,
+        \Magento\Framework\View\Result\PageFactory $pageFactory
+    )
+    {
+        $this->_pageFactory = $pageFactory;
+        $this->api = $api;
+        return parent::__construct($context);
+    }
+
+    public function execute()
+    {
+        return $this->_pageFactory->create();
+    }
+}
