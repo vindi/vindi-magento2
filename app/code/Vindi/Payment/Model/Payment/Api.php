de9988bf8557fafdfd4cd10caf1a1e8778534cbc
leonardo albuquerque <albuquerque.leonardo@trezo.com.br>
2018-07-27 10:55:35 -0300

    adicionado comunicação basica com a api

--- app/code/Vindi/Payment/Model/Payment/Api.php
+++ app/code/Vindi/Payment/Model/Payment/Api.php
@@ -3,35 +3,39 @@
 namespace Vindi\Payment\Model\Payment;
 
 
+use Magento\Framework\Module\ModuleListInterface;
+use Vindi\Payment\Helper\Data;
+
 class Api extends \Magento\Framework\Model\AbstractModel
 {
     private $apiKey;
 
-    public function __construct()
+    public function __construct(
+        Data $helperData,
+        ModuleListInterface $moduleList,
+        \Psr\Log\LoggerInterface $logger,
+        \Magento\Framework\Message\ManagerInterface $messageManager
+    )
     {
-        $this->apiKey = $this->getModuleConfig("api_key");
+        $this->apiKey = $helperData->getModuleConfig("api_key");
+        $this->base_path = $helperData->getModuleConfig("api_endpoint");
 
-//        parent::__construct();
-    }
+        $this->moduleList = $moduleList;
+        $this->logger = $logger;
+        $this->messageManager = $messageManager;
 
-    public function getPlanInstallments($id)
-    {
-        $response = $this->request("plans/{$id}", 'GET');
-        $plan = $response['plan'];
-        $installments = $plan['installments'];
-        return $installments;
     }
 
     private function request($endpoint, $method = 'POST', $data = [], $dataToLog = null)
     {
-        if (!$this->key) {
+        if (!$this->apiKey) {
             return false;
         }
         $url = $this->base_path . $endpoint;
         $body = $this->buildBody($data);
         $requestId = rand();
         $dataToLog = null !== $dataToLog ? $this->buildBody($dataToLog) : $body;
-        $this->log(sprintf("[Request #%s]: Novo Request para a API.\n%s %s\n%s", $requestId, $method, $url,
+        $this->logger->info(sprintf("[Request #%s]: Novo Request para a API.\n%s %s\n%s", $requestId, $method, $url,
             $dataToLog));
         $ch = curl_init();
         $ch_options = [
@@ -42,9 +46,9 @@ class Api extends \Magento\Framework\Model\AbstractModel
             CURLOPT_SSL_VERIFYPEER => true,
             CURLOPT_HEADER => true,
             CURLOPT_RETURNTRANSFER => true,
-            CURLOPT_USERAGENT => 'Vindi-Magento/' . $this->version,
+            CURLOPT_USERAGENT => 'Vindi-Magento/' . $this->getVersion(),
             CURLOPT_SSLVERSION => 'CURL_SSLVERSION_TLSv1_2',
-            CURLOPT_USERPWD => $this->key . ':',
+            CURLOPT_USERPWD => $this->apiKey . ':',
             CURLOPT_URL => $url,
             CURLOPT_CUSTOMREQUEST => $method
         ];
@@ -56,15 +60,15 @@ class Api extends \Magento\Framework\Model\AbstractModel
         $statusCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
         $body = substr($response, curl_getinfo($ch, CURLINFO_HEADER_SIZE));
         if (curl_errno($ch) || $response === false) {
-            $this->log(sprintf("[Request #%s]: Erro ao fazer request!\n%s", $requestId, print_r($response, true)));
+            $this->logger->error(sprintf("[Request #%s]: Erro ao fazer request!\n%s", $requestId, print_r($response, true)));
             return false;
         }
         curl_close($ch);
         $status = "HTTP Status: $statusCode";
-        $this->log(sprintf("[Request #%s]: Nova Resposta da API.\n%s\n%s", $requestId, $status, $body));
+        $this->logger->info(sprintf("[Request #%s]: Nova Resposta da API.\n%s\n%s", $requestId, $status, $body));
         $responseBody = json_decode($body, true);
         if (!$responseBody) {
-            $this->log(sprintf('[Request #%s]: Erro ao recuperar corpo do request! %s', $requestId,
+            $this->logger->info(sprintf('[Request #%s]: Erro ao recuperar corpo do request! %s', $requestId,
                 print_r($body, true)));
             return false;
         }
@@ -72,5 +76,74 @@ class Api extends \Magento\Framework\Model\AbstractModel
             return false;
         }
         return $responseBody;
+    }
+
+    /**
+     * Make an API request to create a Customer.
+     *
+     * @param array $body (name, email, code)
+     *
+     * @return array|bool|mixed
+     */
+    public function createCustomer($body)
+    {
+        if ($response = $this->request('customers', 'POST', $body)) {
+            return $response['customer']['id'];
+        }
+
+        return false;
+    }
+
+    private function buildBody($data)
+    {
+        $body = null;
+
+        if (!empty($data)) {
+            $body = json_encode($data);
+        }
+
+        return $body;
+    }
+
+    public function getVersion()
+    {
+        return $this->moduleList
+            ->getOne('Vindi_Payment')['setup_version'];
+    }
+
+    /**
+     * @param array $response
+     * @param       $endpoint
+     *
+     * @return bool
+     */
+    private function checkResponse($response, $endpoint)
+    {
+        if (isset($response['errors']) && !empty($response['errors'])) {
+            foreach ($response['errors'] as $error) {
+                $message = $this->getErrorMessage($error, $endpoint);
+
+                $this->messageManager->addErrorMessage($message);
+
+                $this->lastError = $message;
+            }
+
+            return false;
+        }
+
+        $this->lastError = '';
+
+        return true;
+    }
+
+    /**
+     * @param array $error
+     * @param       $endpoint
+     *
+     * @return string
+     */
+    private function getErrorMessage($error, $endpoint)
+    {
+        return "Erro em $endpoint: {$error['id']}: {$error['parameter']} - {$error['message']}";
     }
 }
