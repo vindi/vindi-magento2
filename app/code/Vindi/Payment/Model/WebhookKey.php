b35b0b57176fb01765aa70a6e54dd7279c127360
leonardo albuquerque <albuquerque.leonardo@trezo.com.br>
2018-08-08 10:12:27 -0300

    #11549 corrigido tradução de texto #14

--- app/code/Vindi/Payment/Model/WebhookKey.php
+++ app/code/Vindi/Payment/Model/WebhookKey.php
@@ -15,6 +15,6 @@ class WebhookKey implements CommentInterface
 
     public function getCommentText($elementValue)
     {
-        return __("Add this link to the webhooks menu in your vindi dashboard")->getText() . " " . $this->urlInterface->getBaseUrl() . "vindiPayment/index/webhook?key=" . $elementValue;
+        return sprintf(__("Add this link to the webhooks menu in your vindi dashboard")) . " " . $this->urlInterface->getBaseUrl() . "vindiPayment/index/webhook?key=" . $elementValue;
     }
 }
