3f9d251c6376526d931ea7a7c78d89620f5091b0
leonardo lopes de albuquerque <leonardo.lopes.albuquerque@gmail.com>
2018-08-09 19:22:01 -0300

    Criar produto (#5)
    * #11549 removido validação por regex

---
+++ app/code/Vindi/Payment/Model/Payment/Product.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace Vindi\Payment\Model\Payment;
+
+
+class Product
+{
+
+    public function __construct(
+        \Magento\Customer\Api\CustomerRepositoryInterface $customerRepository,
+        Api $api,
+        \Magento\Framework\Message\ManagerInterface $messageManager
+    )
+    {
+        $this->customerRepository = $customerRepository;
+        $this->api = $api;
+        $this->messageManager = $messageManager;
+    }
+
+    public function findOrCreateProducts($order)
+    {
+        foreach ($order->getItems() as $item) {
+            $vindiProductId = $this->findOrCreateProduct($item);
+
+            for ($i = 0; $i < $item->getQtyOrdered(); $i++) {
+                $list[] = [
+                    'product_id' => $vindiProductId,
+                    'pricing_schema' => ['price' => $item->getPrice()]
+                ];
+            }
+        }
+        return $list;
+    }
+
+    private function findOrCreateProduct($item)
+    {
+
+        $vindiProductId = $this->findProductByCode($item->getSku());
+
+        if ($vindiProductId) {
+            return $vindiProductId;
+        }
+
+        $body = [
+            'name' => $item->getName(),
+            'code' => $item->getSku(),
+            'status' => 'active',
+            'pricing_schema' => [
+                'price' => 0,
+            ],
+        ];
+
+        $response = $this->api->request('products', 'POST', $body);
+
+        if ($response) {
+            return $response['product']['id'];
+        }
+
+        return false;
+    }
+
+    private function findProductByCode($code)
+    {
+        $response = $this->api->request("products?query=code%3D{$code}", 'GET');
+
+        if ($response && (1 === count($response['products'])) && isset($response['products'][0]['id'])) {
+            return $response['products'][0]['id'];
+        }
+
+        return false;
+    }
+}
