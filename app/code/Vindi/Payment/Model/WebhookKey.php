a7fa83c78ad0f410a1960887da5945f3dcac0957
laerte-guimaraes <31661772+laerte-guimaraes@users.noreply.github.com>
2018-08-13 15:19:27 -0300

    Ajusta frase dos webhooks

--- app/code/Vindi/Payment/Model/WebhookKey.php
+++ app/code/Vindi/Payment/Model/WebhookKey.php
@@ -15,6 +15,6 @@ class WebhookKey implements CommentInterface
 
     public function getCommentText($elementValue)
     {
-        return sprintf(__("Add this link to the webhooks menu in your vindi dashboard")) . " " . $this->urlInterface->getBaseUrl() . "vindiPayment/index/webhook?key=" . $elementValue;
+        return sprintf(__("Use this link to set up events in Vindi Webhooks.")) . " " . $this->urlInterface->getBaseUrl() . "vindiPayment/index/webhook?key=" . $elementValue;
     }
 }
