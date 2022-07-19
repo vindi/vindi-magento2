f8472515987c86a3cdf99069d12701376b1af6e0
Vinicius Dziuba <dziuba.vinicius@trezo.com.br>
2022-07-19 15:55:17 -0300

    Fix array case not exists in api

--- Model/Payment/AbstractMethod.php
+++ Model/Payment/AbstractMethod.php
@@ -381,7 +381,7 @@ abstract class AbstractMethod extends OriginAbstractMethod
             $payment->setAdditionalInformation('due_at', $bill['charges'][0]['due_at']);
         }
 
-        if ($body['payment_method_code'] === PaymentMethod::PIX) {
+        if ($body['payment_method_code'] === PaymentMethod::PIX && isset($bill['charges'][0]['last_transaction']['gateway_response_fields']['qrcode_original_path'])) {
             $payment->setAdditionalInformation('qrcode_original_path', $bill['charges'][0]['last_transaction']['gateway_response_fields']['qrcode_original_path']);
             $payment->setAdditionalInformation('qrcode_path', $bill['charges'][0]['last_transaction']['gateway_response_fields']['qrcode_path']);
             $payment->setAdditionalInformation('max_days_to_keep_waiting_payment', $bill['charges'][0]['last_transaction']['gateway_response_fields']['max_days_to_keep_waiting_payment']);
