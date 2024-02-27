2e18a8aa936993a828f2cf9f07ba6c021d0f0bde
Thiago Contardi <thiagocontardi@hotmail.com>
2024-02-27 10:58:33 -0300

    fix: remove fpc for the store

--- Controller/Subscription/Index.php
+++ Controller/Subscription/Index.php
@@ -2,8 +2,12 @@
 
 namespace Vindi\Payment\Controller\Subscription;
 
+use Magento\Customer\Model\Session;
 use Magento\Framework\App\Action\Action;
 use Magento\Framework\App\Action\Context;
+use Magento\Framework\App\RequestInterface;
+use Magento\Framework\App\ResponseInterface;
+use Magento\Framework\Exception\NotFoundException;
 use Magento\Framework\View\Result\PageFactory;
 
 /**
@@ -19,15 +23,38 @@ class Index extends Action
     protected $resultPageFactory;
 
     /**
+     * @var Session
+     */
+    protected $customerSession;
+
+    /**
      * @param Context $context
      * @param PageFactory $resultPageFactory
      */
     public function __construct(
         Context $context,
-        PageFactory $resultPageFactory
+        PageFactory $resultPageFactory,
+        Session $customerSession
     ) {
         parent::__construct($context);
         $this->resultPageFactory = $resultPageFactory;
+        $this->customerSession = $customerSession;
+    }
+
+
+    /**
+     * Dispatch request
+     *
+     * @param RequestInterface $request
+     * @return ResponseInterface
+     * @throws NotFoundException
+     */
+    public function dispatch(RequestInterface $request)
+    {
+        if (!$this->customerSession->authenticate()) {
+            $this->_actionFlag->set('', 'no-dispatch', true);
+        }
+        return parent::dispatch($request);
     }
 
     /**
