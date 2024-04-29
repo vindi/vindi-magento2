2d6149dba007aa158de1fd58396f25811b7b6ea1
Iago Cedran <iago@bizcommerce.com.br>
2024-04-29 19:25:02 +0000

    fix: adding PHPStan fixes

--- Plugin/AddCustomOptionToQuoteItem.php
+++ Plugin/AddCustomOptionToQuoteItem.php
@@ -1,152 +1,64 @@
 <?php
+namespace Vindi\Payment\Plugin;
 
-namespace Vindi\Payment\Test\Unit;
-
-use PHPUnit\Framework\TestCase;
-use Magento\Framework\TestFramework\Unit\Helper\ObjectManager;
-use Magento\Customer\Api\CustomerRepositoryInterface;
-use Magento\Framework\Message\ManagerInterface;
-use Magento\Sales\Model\Order;
-use Magento\Sales\Model\Order\Item;
-use Magento\Catalog\Model\Product;
+use Magento\Catalog\Model\Product\Type\AbstractType;
+use Magento\Framework\Exception\LocalizedException;
 
 /**
- * Test class for Order operations in Vindi Payment Module
+ * Class AddCustomOptionToQuoteItem
+ * @package Vindi\Payment\Plugin
  */
-class OrderTest extends TestCase
+class AddCustomOptionToQuoteItem
 {
-    protected ObjectManager $objectManager;
-    protected CustomerRepositoryInterface $customerRepositoryInterface;
-    protected ManagerInterface $managerInterface;
-
     /**
-     * Set up common test elements
+     * Before add product to quote, add custom options if the product has vindi_recurrence_can_show set to 1.
+     *
+     * @param \Magento\Quote\Model\Quote $subject
+     * @param $product
+     * @param \Magento\Framework\DataObject|null $request
+     * @param string $processMode
+     * @return array
+     * @throws LocalizedException
      */
-    protected function setUp(): void
-    {
-        $this->objectManager = new ObjectManager($this);
-        $this->customerRepositoryInterface = $this->getMockBuilder(CustomerRepositoryInterface::class)
-            ->disableOriginalConstructor()
-            ->getMock();
-        $this->managerInterface = $this->getMockBuilder(ManagerInterface::class)
-            ->disableOriginalConstructor()
-            ->getMock();
-    }
+    public function beforeAddProduct(
+        \Magento\Quote\Model\Quote $subject,
+                                   $product,
+                                   $request = null,
+                                   $processMode = AbstractType::PROCESS_MODE_FULL
+    ) {
+        if ($product->getData('vindi_enable_recurrence') == '1') {
+            if ($request instanceof \Magento\Framework\DataObject) {
+                $additionalOptions = [];
 
-    public function testOrderWithTax()
-    {
-        $vindiProductId = 'taxa';
-        $amount = 1.00;
+                $selectedPlanId = $request->getData('selected_plan_id');
+                if (empty($selectedPlanId)) {
+                    throw new LocalizedException(__('A plan must be selected for this product.'));
+                }
 
-        $order = $this->createOrderMock($amount, 0.00, 0.00);
-        $list = $this->createVindiProductManagementMock($vindiProductId)->findOrCreateProductsFromOrder($order);
+                $additionalOptions[] = [
+                    'label' => __('Plan ID'),
+                    'value' => $selectedPlanId,
+                    'code'  => 'plan_id'
+                ];
 
-        $this->makeAssertions($list, $vindiProductId, $amount);
-    }
+                $additionalOptions[] = [
+                    'label' => __('Price'),
+                    'value' => $request->getData('selected_plan_price'),
+                    'code'  => 'plan_price'
+                ];
 
-    public function testOrderWithDiscount()
-    {
-        $vindiProductId = 'cupom';
-        $amount = -5.00;
+                $additionalOptions[] = [
+                    'label' => __('Installments'),
+                    'value' => $request->getData('selected_plan_installments'),
+                    'code'  => 'plan_installments'
+                ];
 
-        $order = $this->createOrderMock(0.00, $amount, 0.00);
-        $list = $this->createVindiProductManagementMock($vindiProductId)->findOrCreateProductsFromOrder($order);
+                if (!empty($additionalOptions)) {
+                    $product->addCustomOption('additional_options', json_encode($additionalOptions));
+                }
+            }
+        }
 
-        $this->makeAssertions($list, $vindiProductId, $amount);
-    }
-
-    public function testOrderWithShipping()
-    {
-        $vindiProductId = 'frete';
-        $amount = 10.00;
-
-        $order = $this->createOrderMock(0.00, 0.00, $amount);
-        $list = $this->createVindiProductManagementMock($vindiProductId)->findOrCreateProductsFromOrder($order);
-
-        $this->makeAssertions($list, $vindiProductId, $amount);
-    }
-
-    private function makeAssertions($list, $vindiProductId, $amount)
-    {
-        $this->assertContains('fake_sku', $list[0]['product_id'], '', true);
-        $this->assertEquals('9.99', $list[0]['amount']);
-        $this->assertEquals($vindiProductId, $list[1]['product_id']);
-        $this->assertEquals($amount, $list[1]['amount']);
-    }
-
-    private function createApiMock($desiredTestResponse = null)
-    {
-        $apiMock = $this->getMockBuilder(\Vindi\Payment\Helper\Api::class)
-            ->disableOriginalConstructor()
-            ->getMock();
-
-        $requestResponses = [
-            ['product' => ['id' => 'fake_sku']],
-            ['products' => [0 => ['id' => $desiredTestResponse]]]
-        ];
-
-        $apiMock->method('request')
-            ->willReturnOnConsecutiveCalls(false, $requestResponses[0], $requestResponses[1]);
-
-        return $apiMock;
-    }
-
-    private function createVindiProductManagementMock($desiredTestResponse)
-    {
-        return $this->objectManager->getObject(\Vindi\Payment\Model\Vindi\ProductManagement::class, [
-            'productRepository' => $this->createVindiProductMock($desiredTestResponse)
-        ]);
-    }
-
-    private function createVindiProductMock($desiredTestResponse)
-    {
-        return $this->objectManager->getObject(\Vindi\Payment\Model\Vindi\Product::class, [
-            'api' => $this->createApiMock($desiredTestResponse)
-        ]);
-    }
-
-    private function createOrderMock(
-        $orderTaxAmount = 0.00, $orderDiscountAmount = 0.00, $orderShippingAmount = 0.00,
-        $itemQty = 1, $itemType = 'simple', $itemPrice = 9.99
-    )
-    {
-        $orderMock = $this->getMockBuilder(Order::class)
-            ->disableOriginalConstructor()
-            ->getMock();
-
-        $items = [$this->createItemMock($itemQty, $itemType, $itemPrice)];
-
-        $orderMock->method('getItems')->willReturn($items);
-        $orderMock->method('getTaxAmount')->willReturn($orderTaxAmount);
-        $orderMock->method('getDiscountAmount')->willReturn($orderDiscountAmount);
-        $orderMock->method('getShippingAmount')->willReturn($orderShippingAmount);
-
-        return $orderMock;
-    }
-
-    private function createItemMock($qty = 1, $type = 'simple', $price = 9.99)
-    {
-        $itemMock = $this->getMockBuilder(Item::class)
-            ->disableOriginalConstructor()
-            ->getMock();
-
-        $itemMock->method('getQtyOrdered')->willReturn($qty);
-        $itemMock->method('getSku')->willReturn('FAKE_SKU');
-        $itemMock->method('getName')->willReturn('FAKE_NAME');
-        $itemMock->method('getPrice')->willReturn($price);
-        $itemMock->method('getProduct')->willReturn($this->createProductMock($type));
-
-        return $itemMock;
-    }
-
-    private function createProductMock($type)
-    {
-        $productMock = $this->getMockBuilder(Product::class)
-            ->disableOriginalConstructor()
-            ->getMock();
-
-        $productMock->method('getTypeId')->willReturn($type);
-
-        return $productMock;
+        return [$product, $request, $processMode];
     }
 }
