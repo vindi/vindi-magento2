51ef0d3019bb8aa0554cd4eb8fdb1c415016f1a6
Iago Cedran <iago@bizcommerce.com.br>
2024-08-09 19:50:34 +0000

    fix: adjusting BillPaid webhook flow for when it is subscription and when it is not

--- Helper/WebHookHandlers/BillPaid.php
+++ Helper/WebHookHandlers/BillPaid.php
@@ -7,6 +7,10 @@ use Vindi\Payment\Model\OrderCreationQueueFactory;
 use Magento\Sales\Model\OrderRepository;
 use Vindi\Payment\Helper\EmailSender;
 use Vindi\Payment\Logger\Logger;
+use Magento\Sales\Model\Order\Invoice;
+use Vindi\Payment\Helper\Data;
+use Magento\Framework\Api\SearchCriteriaBuilder;
+use Magento\Sales\Api\InvoiceRepositoryInterface;
 
 /**
  * Class BillPaid
@@ -49,6 +53,21 @@ class BillPaid
     private $dbAdapter;
 
     /**
+     * @var InvoiceRepositoryInterface
+     */
+    private $invoiceRepository;
+
+    /**
+     * @var SearchCriteriaBuilder
+     */
+    private $searchCriteriaBuilder;
+
+    /**
+     * @var Data
+     */
+    private $helperData;
+
+    /**
      * Constructor for initializing class dependencies.
      */
     public function __construct(
@@ -58,7 +77,10 @@ class BillPaid
         OrderCreationQueueFactory $orderCreationQueueFactory,
         OrderRepository $orderRepository,
         EmailSender $emailSender,
-        \Magento\Framework\App\ResourceConnection $resourceConnection
+        \Magento\Framework\App\ResourceConnection $resourceConnection,
+        InvoiceRepositoryInterface $invoiceRepository,
+        SearchCriteriaBuilder $searchCriteriaBuilder,
+        Data $helperData
     ) {
         $this->logger = $logger;
         $this->orderCreator = $orderCreator;
@@ -67,6 +89,9 @@ class BillPaid
         $this->orderRepository = $orderRepository;
         $this->emailSender = $emailSender;
         $this->dbAdapter = $resourceConnection->getConnection();
+        $this->invoiceRepository = $invoiceRepository;
+        $this->searchCriteriaBuilder = $searchCriteriaBuilder;
+        $this->helperData = $helperData;
     }
 
     /**
@@ -84,8 +109,27 @@ class BillPaid
             return false;
         }
 
+        $isSubscription = isset($bill['subscription']) && $bill['subscription'] != null;
+
+        if ($isSubscription) {
+            return $this->handleSubscriptionFlow($bill, $data);
+        } else {
+            return $this->handleRegularOrderFlow($bill);
+        }
+    }
+
+    /**
+     * Handle the subscription flow.
+     *
+     * @param array $bill
+     * @param array $data
+     * @return bool
+     */
+    private function handleSubscriptionFlow($bill, $data)
+    {
         $subscriptionId = $bill['subscription']['id'];
         $lockName = 'vindi_subscription_' . $subscriptionId;
+
         if (!$this->dbAdapter->query("SELECT GET_LOCK(?, 10)", [$lockName])->fetchColumn()) {
             $this->logger->error(__('Could not acquire lock for subscription ID: %1', $subscriptionId));
             return false;
@@ -110,5 +154,97 @@ class BillPaid
         } finally {
             $this->dbAdapter->query("SELECT RELEASE_LOCK(?)", [$lockName]);
         }
+    }
+
+    /**
+     * Handle the regular order flow.
+     *
+     * @param array $bill
+     * @return bool
+     */
+    private function handleRegularOrderFlow($bill)
+    {
+        $order = null;
+
+        if (isset($bill['code']) && $bill['code'] != null) {
+            $order = $this->getOrder($bill['code']);
+        }
+
+        if (!$order) {
+            $this->logger->error(__('Order not found for bill code: %1', $bill['code']));
+            return false;
+        }
+
+        return $this->createInvoice($order);
+    }
+
+    /**
+     * Create an invoice for a regular order.
+     *
+     * @param \Magento\Sales\Model\Order $order
+     * @return bool
+     * @throws \Magento\Framework\Exception\LocalizedException
+     */
+    public function createInvoice(\Magento\Sales\Model\Order $order)
+    {
+        if (!$order->getId()) {
+            return false;
+        }
+
+        $this->logger->info(__('Generating invoice for the order %1.', $order->getId()));
+
+        if (!$order->canInvoice()) {
+            $this->logger->error(__('Impossible to generate invoice for order %1.', $order->getId()));
+            return false;
+        }
+
+        $invoice = $order->prepareInvoice();
+        $invoice->setRequestedCaptureCase(Invoice::CAPTURE_OFFLINE);
+        $invoice->register();
+        $invoice->pay();
+        $invoice->setSendEmail(true);
+        $this->invoiceRepository->save($invoice);
+
+        $this->logger->info(__('Invoice created successfully.'));
+
+        $status = $this->helperData->getStatusToPaidOrder();
+        if ($state = $this->helperData->getStatusState($status)) {
+            $order->setState($state);
+        }
+
+        $order->addCommentToStatusHistory(
+            __('The payment was confirmed and the order is being processed'),
+            $status
+        );
+
+        $this->orderRepository->save($order);
+
+        return true;
+    }
+
+    /**
+     * Retrieve the order by increment ID.
+     *
+     * @param string $incrementId
+     * @return \Magento\Sales\Api\Data\OrderInterface|false
+     */
+    private function getOrder($incrementId)
+    {
+        $searchCriteria = $this->searchCriteriaBuilder
+            ->addFilter('increment_id', $incrementId, 'eq')
+            ->create();
+
+        $orderList = $this->orderRepository
+            ->getList($searchCriteria)
+            ->getItems();
+
+        try {
+            return reset($orderList);
+        } catch (\Exception $e) {
+            $this->logger->error(__('Order #%1 not found', $incrementId));
+            $this->logger->error($e->getMessage());
+        }
+
+        return false;
     }
 }
